d5613af3ff1831f355f3d3bc9fde249e3b7b8eff
Subash KC <subash.kc@tlgcohort.com>
2020-09-01 08:20:48 -0800

    server changed

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const express = require("express");
 const server = express();
 
-server.listen(4000, () => {
+let PORT = process.env.PORT ? process.env.PORT : 4000;
+
+server.listen(PORT, () => {
   console.log("server listening");
 });
